83dd5a91fd027e8bd99f01f5db29261c236d8b17
jesus arias <jesusg94av@gmail.com>
2021-09-12 23:07:48 -0300

    version final de twitterÂ

---
+++ app/models/tweet.rb
@@ -0,0 +1,87 @@
+include ActionView::Helpers::UrlHelper
+
+class Tweet < ApplicationRecord
+  before_save :add_hashtags
+
+  belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liking_users, :through => :likes, :source => :user
+
+  validates :content, :presence => true
+
+  paginates_per 50
+  
+  scope :tweets_for_me, ->(user) { where(user_id: user.arr_friends_id_and_me) }
+
+  def add_hashtags
+    new_content = ""
+    self.content.split(" ").each do |word|
+      if word.start_with?("#")
+        word_clean = word.gsub("#", "")
+        new_content += link_to(word, Rails.application.routes.url_helpers.root_path+"?search="+word_clean)+" "
+      else 
+        new_content += word + " "
+      end 
+    end 
+    self.content = new_content 
+  end        
+
+  def like_icon(user)
+    if self.is_liked(user)
+      'heart'
+    else 
+      'heart-broken'
+    end 
+  end     
+  
+  def retweet_color
+    self.count_rt > 0 ? 'danger' : 'dark' 
+  end  
+
+  def is_liked(user)
+    self.liking_users.include?(user)
+  end
+
+  # def liking_users
+  #   Like.where(tweet_id:self.id).pluck(:user_id)
+  # end 
+
+  def like(user)
+     Like.create(user_id: user.id, tweet_id: self.id)
+  end  
+
+  def remove_like(user)
+    Like.where(user_id: user, tweet_id: self).destroy_all
+  end 
+
+  def count_rt
+    Tweet.where(rt_ref: self.id).count
+  end
+  
+  def is_retweet?
+    self.rt_ref.present?
+  end
+  
+  def tweet_ref
+    Tweet.find(self.rt_ref)
+  end  
+  def add_hashtags
+    new_content = ""
+    self.content.split(" ").each do |word|
+      if word.start_with?("#")
+        #new_content += "#{word}"
+        word_clean = word.gsub("#", "")
+        new_content += '<a href="/?search=#'+word_clean+'">'+word+'</a>'
+        #link_to(word, Rails.application.routes.url_helpers.root_path+"?search="+word_clean)
+
+      else
+        new_content += word + " "        
+      end
+    end
+
+    self.content = new_content
+
+  end
+
+end  
+
---
+++ db/migrate/seeds.rb
@@ -0,0 +1,43 @@
+require 'faker'
+
+User.create([{
+    name: 'Mikey Manjiro  Sano',
+    photo: 'https://ramenparados.com/wp-content/uploads/2020/12/EpWfGheVoAAuDN3.jpg',
+    email: 'toman_mikey@mail.com',
+    password: '123123',
+    password_confirmation: '123123'
+},{
+    name: 'Draken Ken Ryuguji',
+    photo: 'https://ramenparados.com/wp-content/uploads/2020/12/EpWfTY8UYAAUrAr.jpg',
+    email: 'toman_draken@mail.com',
+    password: '123123',
+    password_confirmation: '123123'
+},{
+    name: 'baji keisuke',
+    photo: 'https://ramenparados.com/wp-content/uploads/2021/03/EwRVa-tUcAEsgaF.png',
+    email: 'toman_bajin@mail.com',
+    password: '123123',
+    password_confirmation: '123123'
+},{
+    name: 'mitsuya takashi',
+    photo: 'https://ramenparados.com/wp-content/uploads/2021/03/EwRVeJtVkAQwWo7.png',
+    email: 'toman_mitsuya@mail.com',
+    password: '123123',
+    password_confirmation: '123123'
+},{
+    name: 'pachin haruki hayashida',
+    photo: 'https://ramenparados.com/wp-content/uploads/2021/03/EwRVoREUUAAm5b9.png',
+    email: 'toman_pachin@mail.com',
+    password: '123123',
+    password_confirmation: '123123'}])
+
+
+.times do
+    Tweet.create(
+        content: Faker::TvShows::TokioRevengres.quote,
+        user_id: rand(1..5)
+    ) 
+end
+
+AdminUser.create!(email: 'admin@mail.com', password: '123123', password_confirmation: '123123') if Rails.env.development?
+AdminUser.create!(email: 'admin@mail.com', password: '123123', password_confirmation: '123123') if Rails.env.production?
